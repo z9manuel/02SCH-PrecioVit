4cafd934aa9db36feb4ef17d7e3424570ce07b04
José Manuel Rodríguez Morales <z9manuel@yahoo.com.mx>
2020-07-29 21:18:43 -0500

    Add project files.

---
+++ 02SCH-PrecioVit/02SCH-PrecioVit.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		_02SCH_PrecioVit.ino
+ Created:	7/29/2020 9:17:35 PM
+ Author:	mrodriguez
+ Programa para despliegue de pracios en Vitrinas SUCAHERSA
+*/
+
+
+void setup() {
+
+}
+
+
+void loop() {
+  
+}
